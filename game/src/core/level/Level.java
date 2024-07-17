d1435b254868a15b00895149f758924bed77f620
Finn Watermann <finn.watermann@hsbi.de>
2024-07-17 17:31:20 +0200

    Rename methods

--- game/src/core/level/Level.java
+++ game/src/core/level/Level.java
@@ -9,43 +9,43 @@ import de.fwatermann.dungine.utils.annotations.Null;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
 
-public class Level {
+public abstract class Level {
 
   private final LevelChunkTree chunks = new LevelChunkTree();
   protected TextureAtlas textureAtlas;
   protected IGenerator generator;
 
-  public Level() {
+  protected Level() {
     this.textureAtlas = new TextureAtlas();
   }
 
   @Null
-  public Block getTileAt(int x, int y, int z) {
+  public Block blockAt(int x, int y, int z) {
     LevelChunk chunk = this.getChunkAtPosition(x, y, z);
-    if(chunk == null) return null;
+    if (chunk == null) return null;
     return chunk.getBlockAt(x, y, z);
   }
 
-  public final Block getTileAt(Vector3i position) {
-    return this.getTileAt(position.x, position.y, position.z);
+  public final Block blockAt(Vector3i position) {
+    return this.blockAt(position.x, position.y, position.z);
   }
 
-  public final Block getTileAt(float x, float y, float z) {
-    return this.getTileAt((int) x, (int) y, (int) z);
+  public final Block blockAt(float x, float y, float z) {
+    return this.blockAt((int) x, (int) y, (int) z);
   }
 
-  public final Block getTileAt(Vector3f position) {
-    return this.getTileAt(position.x, position.y, position.z);
+  public final Block blockAt(Vector3f position) {
+    return this.blockAt(position.x, position.y, position.z);
   }
 
-  public final  LevelChunk getChunkAtPosition(int x, int y, int z) {
+  public final LevelChunk getChunkAtPosition(int x, int y, int z) {
     Vector3i chunkCoordinates = ChunkUtils.toChunkCoordinates(x, y, z);
     return this.getChunk(chunkCoordinates.x, chunkCoordinates.y, chunkCoordinates.z);
   }
 
-  public final  LevelChunk getChunk(int chunkX, int chunkY, int chunkZ) {
+  public final LevelChunk getChunk(int chunkX, int chunkY, int chunkZ) {
     LevelChunk chunk = this.chunks.find(chunkX, chunkY, chunkZ);
-    if(chunk == null) {
+    if (chunk == null) {
       chunk = this.generator.generateChunk(chunkX, chunkY, chunkZ);
       this.chunks.insert(chunk);
     }
@@ -53,9 +53,9 @@ public class Level {
   }
 
   public void render(CameraPerspective camera) {
-    this.chunks.forEach(chunk -> {
-      chunk.render(camera);
-    });
+    this.chunks.forEach(
+        chunk -> {
+          chunk.render(camera);
+        });
   }
-
 }
